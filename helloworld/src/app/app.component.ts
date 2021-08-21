f04413798fcb7dd67b5bb59daa1dcfa9df591eb2
kunallad54 <kunallad5473@gmail.com>
2021-08-21 21:03:55 +0530

    Ability to Bind Bridgelabz Website with Logo using event binding

--- helloworld/src/app/app.component.ts
+++ helloworld/src/app/app.component.ts
@@ -8,9 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: string = 'helloworld';
   imgUrl="";
+  url="";
+  
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz";
     this.imgUrl = "../assets/Bridgelabz-logo.jpg"
+    this.url = "https://www.bridgelabz.com";
   } 
+
+  OnClick($event: any) {
+    console.log("Save Button is Clicked",$event);
+    window.open(this.url,"_blank");
+  }
+  
 }
